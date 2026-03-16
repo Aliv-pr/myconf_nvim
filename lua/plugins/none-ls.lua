d19cd5c8c00bc56442e053fed9dccbe9e3ddec48
Aliv-pr <alex2408rosales@gmail.com>
2026-03-16 16:23:17 -0600

    Instalación de prueba en none-ls para instalar rubocop

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,19 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local b = null_ls.builtins
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local b = null_ls.builtins
 
-    null_ls.setup({
-      sources = {
-        b.formatting.stylua,
-        b.completion.spell,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf",
-    function()
-      vim.lsp.buf.format({ async = true })
-    end, { desc = "Format file" })
-  end,
+		null_ls.setup({
+			sources = {
+				b.formatting.stylua,
+				b.completion.spell,
+				b.diagnostics.rubocop,
+				b.formatting.rubocop,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", function()
+			vim.lsp.buf.format({ async = true })
+		end, { desc = "Format file" })
+	end,
 }
